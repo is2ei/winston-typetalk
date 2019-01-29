d6af0b5b9008c94aeaf095f94e9958bef114cfc2
Issei Horie <issei.horie@nulab-inc.com>
2019-01-30 02:27:05 +0900

    Fix code style

--- test/typetalk.test.js
+++ test/typetalk.test.js
@@ -1,5 +1,8 @@
 const typetalkLogger = require('../lib/typetalk');
 
 describe('typetalk', function () {
-    
+
+  describe('constructed', function() {
+    typetalkLogger.name === 'typetalk';
+  });
 });
